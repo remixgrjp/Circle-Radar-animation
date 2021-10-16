54d86547d431896a93f00b69b0ead272fb684458
remix <http+github.com@remix.asia>
2021-10-16 14:49:40 +0900

    Empty Activity / Android Studio 4.2.2

---
+++ app/src/main/java/asia/remix/radar2/MainActivity.java
@@ -0,0 +1,14 @@
+package asia.remix.radar2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState ){
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_main );
+	}
+}
